2f6f1f56d467b39eb5b81f5450976c8f928f5e87
Ewande <michal.augoff@gmail.com>
2015-07-23 15:37:15 -0400

    new role setup

---
+++ AntzVS/Role.h
@@ -0,0 +1,23 @@
+/*
+* Antz
+* Antz Framework
+* Role.h
+*
+* Copyright (c) 2015 Zhi Xing. All rights reserved.
+* License: <#license#>
+*/
+
+
+#ifndef __Antz__Role__
+#define __Antz__Role__
+
+namespace Antz
+{
+	class Role
+	{
+		virtual int test() = 0;
+	};
+
+}
+
+#endif
