00afb5a1da22f19005beadaa91ebcc67e19e970a
eli goldweber <eligoldweber@gmail.com>
2015-08-21 16:38:04 -0400

    old cardinality version

--- AntzVS/GuiderRole.cpp
+++ AntzVS/GuiderRole.cpp
@@ -3,7 +3,7 @@
 using namespace Antz;
 
 GuiderRole::GuiderRole(SmartBot& _robot)
-    : Role(_robot), recalculation(false), foodTimer(0), nestTimer(0), priority(DEFAULT_PRIORITY), lowFoodDir(-1), lowNestDir(-1)
+    : Role(_robot), recalculation(false), foodTimer(0), nestTimer(0), priority(DEFAULT_PRIORITY)
 {
     robot.stopMoving();
     robot.wipingNeighborsTimer = NEIGHBORS_COLLECTION_TIME_GUID;
@@ -23,11 +23,9 @@ int GuiderRole::makeStep()
 {
     if(robot.wipingNeighborsTimer % 3 == 0)
     {
-      
-
         //makeTurn();
-    //robot.minFood = NO_SIGNAL;      // to store the minimum food cardinality
-    //robot.minNest = NO_SIGNAL;      // to store the minimum nest cardinality
+    robot.minFood = NO_SIGNAL;      // to store the minimum food cardinality
+    robot.minNest = NO_SIGNAL;      // to store the minimum nest cardinality
     }
   
 //    Reposcount ++;
@@ -37,27 +35,26 @@ int GuiderRole::makeStep()
 //      makeTurn();
 //      Reposcount = 0;
 //    }
-    
+
 
     Display& display = robot.display;
     display.listeningForSignals(); // when red LED is on, the robot listens to the signals from surroundings
 
-    bool wait = true;	// a flag indicating whether there're more signals to be heard
+    bool wait = true; // a flag indicating whether there're more signals to be heard
     int roleDecision = NO_SWITCH;
     /* keep looping until message is heard */
  //Commented out to act as stationarly beacon with no role changes **********
 //    if(robot.curFood == NO_SIGNAL) //if the food is seen, only send signal
 //    {
-    //while (roleDecision == NO_SWITCH /*&& (wait || robot.minNest == NO_SIGNAL && robot.minFood == NO_SIGNAL)*/)
+    while (roleDecision == NO_SWITCH && (wait || robot.minNest == NO_SIGNAL && robot.minFood == NO_SIGNAL))
         wait = receiveSignal(roleDecision);
 
-  Serial.print("wipe Neighbor Timer :");
-  Serial.println(robot.wipingNeighborsTimer);
+
   robot.wipingNeighborsTimer--;
     //}
 /////////
-//while (roleDecision == NO_SWITCH && (wait || robot.minNest == NO_SIGNAL && robot.minFood == NO_SIGNAL))
-//        wait = receiveSignal(roleDecision);
+while (roleDecision == NO_SWITCH && (wait || robot.minNest == NO_SIGNAL && robot.minFood == NO_SIGNAL))
+        wait = receiveSignal(roleDecision);
     
     if (roleDecision == NO_SWITCH)
     {
@@ -94,7 +91,7 @@ int GuiderRole::makeStep()
 bool GuiderRole::receiveSignal(int& roleDecision)
 {
     bool received = false;
-    unsigned long cur = millis();	//millis() returns the number of milliseconds since the arduino began to run this program
+    unsigned long cur = millis(); //millis() returns the number of milliseconds since the arduino began to run this program
     if (cur - nestTimer > 10000)
         robot.curNest = NO_SIGNAL;
     if (cur - foodTimer > 10000)
@@ -106,36 +103,21 @@ bool GuiderRole::receiveSignal(int& roleDecision)
         if (robot.recver.canHearSignal(i))
         {
                   Serial.print("Receiving from sensor : ");
-        Serial.print(i);
+        Serial.println(i);
             received = true;
             uint32_t receivedSignal; // to store the 32-bit signal 
             if (robot.recver.recvFrom(i, &receivedSignal))
-            {	
+            { 
                 Neighbor* currentN = new Neighbor(receivedSignal);
-                Serial.print("Neighbor (");
-                Serial.print(robot.isNeighborValid(*currentN));
-                Serial.print("): ");
-                Serial.print(currentN->id);
-                Serial.print("    ");
-                Serial.print(currentN->curFood);
-                Serial.print("    ");
-                Serial.println(currentN->curNest);
-                if(!robot.isNeighborValid(*currentN))
-                {
-                  Serial.print("curFood: ");
-                  Serial.print(robot.curFood);
-                  Serial.print(", curNest: ");
-                  Serial.println(robot.curNest);
-                }
+                
                 /*Serial.print("Received: ");
                 Serial.println(currentN->id);
                 Serial.println(currentN->orgSignal, BIN);*/
                 if (robot.isNeighborValid(*currentN))
                 {
                     //Serial.println("validated");
-                    //lowFoodDir = i;
-                    //robot.minFood = min(currentN->curFood, robot.minFood);
-                    //robot.minNest = min(currentN->curNest, robot.minNest);
+                    robot.minFood = min(currentN->curFood, robot.minFood);
+                    robot.minNest = min(currentN->curNest, robot.minNest);
                     robot.registerRobotSignal(*currentN, i);
                 }
                 else
@@ -148,39 +130,8 @@ bool GuiderRole::receiveSignal(int& roleDecision)
     {
         
         robot.formNeighborhood();
-
-        int newLowFoodDir;
-        int newLowNestDir;
-        
-        Neighbor* lowestFoodNeighbor = robot.getLowestCardNeighbor(TARGET_FOOD, &newLowFoodDir);
-        Neighbor* lowestNestNeighbor = robot.getLowestCardNeighbor(TARGET_NEST, &newLowNestDir);
-
-        if(lowFoodDir < 0 || (newLowFoodDir + 1) % 6 == lowFoodDir || newLowFoodDir == lowFoodDir || (newLowFoodDir - 1 + 6) % 6 == lowFoodDir 
-           || lowestFoodNeighbor->curFood < robot.minFood)
-        {
-            robot.minFood = lowestFoodNeighbor->curFood;
-            lowFoodDir = newLowFoodDir;
-        }
-        else
-            robot.minFood = NO_SIGNAL;
-
-        if(lowNestDir < 0 || (newLowNestDir + 1) % 6 == lowNestDir || newLowNestDir == lowNestDir || (newLowNestDir - 1 + 6) % 6 == lowNestDir
-           || lowestNestNeighbor->curNest < robot.minNest)
-        {
-            lowNestDir = newLowNestDir;
-            robot.minNest = lowestNestNeighbor->curNest;
-        }
-        else
-            robot.minNest = NO_SIGNAL;
-            
-        
-        
-        
-        
-        //lowFoodDir = 
-        
-        //Display& display = robot.display;
-        //display.number(true, robot.countNeighbors());
+//        Display& display = robot.display;
+//        display.number(true, robot.countNeighbors());
         
         if (robot.countNeighbors() >= 3)
         {
@@ -190,20 +141,18 @@ bool GuiderRole::receiveSignal(int& roleDecision)
                 recalculation = true;
                 Serial.println("Recalculating...");
                 delay(random(1000));
-                
+                robot.wipingNeighborsTimer =  NEIGHBORS_COLLECTION_TIME_GUID; // same as 1st check
             }
             else
                 roleDecision = SWITCH_ROLE;
-                
            
         }
         else
         {
             recalculation = false;
-            //robot.wipingNeighborsTimer = NEIGHBORS_COLLECTION_TIME_GUID;
+            robot.wipingNeighborsTimer = NEIGHBORS_COLLECTION_TIME_GUID;
         }
-
-        robot.wipingNeighborsTimer =  NEIGHBORS_COLLECTION_TIME_GUID; // same as 1st check
+        
         robot.wipeNeighbors();
         //Serial.println("-----------------------------------------");
     }
